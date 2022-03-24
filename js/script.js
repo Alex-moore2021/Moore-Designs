95d751684573424a297916c4a1b1e8024ef17e66
Alex M <84628090+Alex-moore2021@users.noreply.github.com>
2022-03-24 04:59:14 -0700

    commit

--- js/script.js
+++ js/script.js
@@ -14,7 +14,6 @@ console.log(h1);
 // Set current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
-console.log(currentYear);
 yearEl.textContent = currentYear;
 
 // Make mobile navigation work
@@ -24,7 +23,7 @@ const headerEl = document.querySelector(".header");
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
-})("nav-open");
+});
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
